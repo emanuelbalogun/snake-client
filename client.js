ef549df16294471c6d6188884a7ff3437c4032ec
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-20 09:32:03 -0500

    Send Client Initial

--- client.js
+++ client.js
@@ -9,11 +9,15 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on("connect",()=> {
+    console.log("Successfully connected to game server");
+    conn.write("Name:BAE");
+  });
 
   conn.on("data",(data) => {
     console.log(data);
 
-  })
+  });
   return conn;
 };
 

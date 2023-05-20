3f9f786a8676cd3e6b777c0f34499ab88b542f57
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-20 18:06:59 -0500

    Implement the setupInput method

--- play.js
+++ play.js
@@ -1,2 +1,11 @@
 const connect = require("./client");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};

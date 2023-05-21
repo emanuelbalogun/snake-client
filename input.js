61aa2159caabb2952b2fb8587e2ace7926d856f5
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-21 18:58:31 -0500

    Implement movement

--- input.js
+++ input.js
@@ -1,18 +1,42 @@
-const setupInput = function () {
+let connection;
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
+  stdin.on("keypress", handleUserInput);
   return stdin;
 };
 
 const handleUserInput = (key) => {
-  // your code here
-  console.log(key);
-  if (key === '\u0003') {
+  // your code here\
+
+  switch (key) {
+  case "w":
+    connection.write("Move: up");
+    break;
+  case "a":
+    connection.write("Move: left");
+    break;
+  case "s":
+    connection.write("Move: down");
+    break;
+  case "d":
+    connection.write("Move: right");
+    break;
+  }
+
+  // console.log(key);
+  //if(key === '\u00057' || key === '\u00077')
+  if (key === "\u0003") {
     process.exit();
   }
 };
 
-module.exports = setupInput;
+//\u0057 W U+0077
+setupInput();
+module.exports = { setupInput };
+
+
--- play.js
+++ play.js
@@ -1,20 +1,4 @@
 const connect = require("./client");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
+const { setupInput } = require("./input");
 connect();
 setupInput();

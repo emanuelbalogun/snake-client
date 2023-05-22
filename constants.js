68ddbda0c85e22f34b006bd18039adf220de1d51
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-21 21:03:33 -0500

    Add new constants

--- constants.js
+++ constants.js
@@ -1,3 +1,10 @@
 const IP = "172.19.168.205";
 const PORT = 50541;
-module.exports = { IP, PORT };
+
+const MOVEKEY = {
+  MOVE_UP_KEY: "w",
+  MOVE_KEY_LEFT: "a",
+  MOVE_KEY_DOWN: "S",
+  MOVE_KEY_RIGHT: "d",
+};
+module.exports = { IP, PORT, MOVEKEY };

5204a5bc3de43fcd8d256c671e1c1a5c908530e4
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-26 08:27:18 -0500

    Correct to small letter

--- constants.js
+++ constants.js
@@ -1,10 +1,10 @@
-const IP = "172.19.168.205";
+const IP = "172.19.162.190";
 const PORT = 50541;
 
 const MOVEKEY = {
   MOVE_UP_KEY: "w",
   MOVE_KEY_LEFT: "a",
-  MOVE_KEY_DOWN: "S",
+  MOVE_KEY_DOWN: "s",
   MOVE_KEY_RIGHT: "d",
 };
 module.exports = { IP, PORT, MOVEKEY };

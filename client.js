97ca5a8465f629e6aa05236638cd81f9bd67fdad
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-29 19:56:39 -0500

    Refactor the code to remove unsed code

--- client.js
+++ client.js
@@ -9,14 +9,13 @@ const connect = function() {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  conn.on("connect",()=> {
+  conn.on("connect",() => {
     console.log("Successfully connected to game server");
     conn.write("Name:BAE");
   });
 
   conn.on("data",(data) => {
     console.log(data);
-
   });
   return conn;
 };
